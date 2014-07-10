3f229c57239d130ac07b5efcaf5f6c1e92cc15b1
Chris Barrieau <Chris.Barrieau@hotmail.com>
2014-07-09 21:26:19 -0400

    tbest
    Chris test 1

---
+++ webroot/notindex.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+	<title>ProjectHOT</title>
+</head>
+<body>
+<h1>ProjectHOT</h1>
+<p>ProjectHOT is the web version of what will hopefully become a Pokemon TCG AI that will then grow to draw conclusions and suggest builds.</p>
+<p>The project will likely follow these first draft phases:
+<ol>
+	<li>Back end: Understand the rules of the game</li>
+    <li>Back end: Understand the cards' structures</li>
+    <li>Back end: Have a database of the current metagame ofo cards</li>
+    <li>Make the AI</li>
+    <li>Run the AI to come up with "perfect plays" for decks, along with the odds of pulling one off. Produce bell curves to suggest how to play.</li>
+    <li>Have the AI run thousands of test games simultaneously to give matchup odds. Use the AI toolbox to spit out why certain decks beat others.</li>
+    <li>Finally, use the AI to suggest deck builds and practice against it</li>
+</ol>
+</p>
+<?php
+	phpInfo();
+?>
+</body>
+</html>
