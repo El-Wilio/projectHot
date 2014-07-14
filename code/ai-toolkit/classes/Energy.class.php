9291efa021db1c04b8818f335c1345eace8fbda0
Chris Barrieau <Chris.Barrieau@hotmail.com>
2014-07-14 14:23:34 -0400

    ai-toolkit restructured - for realz this time

---
+++ code/ai-toolkit/classes/Energy.class.php
@@ -0,0 +1,33 @@
+<?php
+
+/************************************************************************************** |
+ * Class: 				Energy															|
+ * Date Created: 		2014-07-09														|
+ * Author: 				Chris Barrieau													|
+ *																						|
+ * Model Class																			|
+ * This class represents one Energy, "within" a card. It should have attributes 		|
+ * and methods to allow for any instantiated object to replicate any Energy card in		|
+ * format.																				|
+ *																						|
+ * Modifications will be made accordingly later on in order to allow for compatibility 	|
+ * with previous formats. 																|
+ *																						|
+ * First format allowed: 2014-2015, NXD-ON												|
+ *																						|
+ * Modification History																	|
+ * Programmer		|	Date		|	Details											|
+ * -------------------------------------------------------------------------------------|
+ *																						|
+ ***************************************************************************************/
+class Energy {
+	//attributes
+	
+	
+	function __construct() {
+		
+	}
+		
+}
+
+?>
