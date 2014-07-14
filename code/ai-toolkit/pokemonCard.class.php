db17b291da6e17243957e731d0dacf066b85de5c
William Wright <william.alexandre.wright@gmail.com>
2014-07-14 14:26:35 -0400

    updated card class

--- code/ai-toolkit/pokemonCard.class.php
+++ code/ai-toolkit/pokemonCard.class.php
@@ -16,9 +16,28 @@
  *																						|
  * Modification History
  *
+ */
+
+
+
+abstract class EnergyType {
+   const Colorless   = 0;
+   const Darkness    = 1;
+   const Dragon      = 2;
+   const Fairy       = 3;
+   const Fighting    = 4;
+   const Fire        = 5;
+   const Lightning   = 6;
+   const Metal       = 7;
+   const Psychic     = 8;
+   const Water       = 9;          
+
 class PokemonCard {
 	//attributes
+
+    
 	
+    class 
 	
 	function __construct() {
 		
