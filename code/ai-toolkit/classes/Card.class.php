79085d3c7c38e683200a23b637b17604c22f58f9
William Wright <william.alexandre.wright@gmail.com>
2014-07-16 02:47:27 -0400

    update Card.class.php

--- code/ai-toolkit/classes/Card.class.php
+++ code/ai-toolkit/classes/Card.class.php
@@ -6,28 +6,54 @@ include 'EnergyType.ENUM.php';
 abstract class Card {
 	//attributes
 
-    private $id; //unneededd for the toolkit. That's specific to the database.
     private $name;
+    private $set;
+    private $setNumber;
+    private $rarity;
+
 	//Missing: Set, set number, rarity
 	
-	function __construct($id, string $name) {
-	    $this->$format = $format;
-        $this->$name =$name;
+	function __construct(string $name, string $set, int $setNumber, int $rarity) {
+        setName($name);
+        setSet($set);
+        setSetNumber($setNumber);
+        setRarity($rarity);
     }
 
-    public function getFormat() {
-        return $this->$format
-    }
-	
     public function getName() {
-        return $this->name
+        return $this->$name;
     }
 
-    public function setFormat($format) {
-        $this->$format = $format;
+    public function getSet() {
+        return $this->$set;
+    }
+
+    public function getSetNumber() {
+        return $this->$setNumber;
+    }
+
+    public function getRarity() {
+        return $this->$rarity;
+    }
+
+    #todo throw error is parameter is not a string
+    public function setName($name) {
         $this->$name = $name;
     }
-		
-}
+
+    #todo learn wtf is a set
+    public function setSet($set) {
+        $this->$set = $set;
+    }
+
+    #todo learn wtf is a set number
+    public function setSetNumber($setNumber) {
+        $this->$setNumber = $setNumber;
+    }
+
+    #todo learn more about this rarity, what chris actually means?
+    public function setRarity($rarity) {
+        $this->$rarity = $rarity;
+    }
 
 ?>
