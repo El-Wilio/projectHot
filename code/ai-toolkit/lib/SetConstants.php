980ea03d94437c79b03dbe0ba1f84eaf899937c6
William Wright <william.alexandre.wright@gmail.com>
2014-07-31 14:32:56 -0400

    added a library file that contains all the set.

---
+++ code/ai-toolkit/lib/SetConstants.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Set;
+
+define(__NAMESPACE__."_1", "Base Set");
+define(__NAMESPACE__."_2", "Jungle");
+define(__NAMESPACE__."_3", "Fossil");
+define(__NAMESPACE__."_4", "Base Set 2");
+define(__NAMESPACE__."_5", "Team Rocket");
+define(__NAMESPACE__."_6", "Gym Heroes");
+define(__NAMESPACE__."_7", "Gym Challenge");
+define(__NAMESPACE__."_8", "Neo Genesis");
+define(__NAMESPACE__."_9", "Neo Discovery");
+define(__NAMESPACE__."_10", "Neo Revelation");
+define(__NAMESPACE__."_11", "Neo Destiny");
+define(__NAMESPACE__."_12", "Legendary Collection");
+define(__NAMESPACE__."_13", "Expedition Base Set");
+define(__NAMESPACE__."_14", "Aquapolis");
+define(__NAMESPACE__."_15", "Skyridge");
+define(__NAMESPACE__."_16", "EX Ruby & Sapphire");
+define(__NAMESPACE__."_17", "EX Sandstorm");
+define(__NAMESPACE__."_18", "EX Dragon");
+define(__NAMESPACE__."_19", "EX Team Magma vs Team Aqua");
+define(__NAMESPACE__."_20", "EX Hidden Legends");
+define(__NAMESPACE__."_21", "EX FireRed & LeafGreen");
+define(__NAMESPACE__."_22", "EX Team Rocket Returns");
+define(__NAMESPACE__."_23", "EX Deoxys");
+define(__NAMESPACE__."_24", "EX Emerald");
+define(__NAMESPACE__."_25", "EX Unseen Forces")
+define(__NAMESPACE__."_26", "EX Delta Species");
+define(__NAMESPACE__."_27", "EX Legend Maker");
+define(__NAMESPACE__."_28", "EX Holon Phantoms");
+define(__NAMESPACE__."_29", "EX Crystal Guardians");
+define(__NAMESPACE__."_30", "EX Dragon Frontiers");
+define(__NAMESPACE__."_31", "EX Power Keepers");
+define(__NAMESPACE__."_32", "Diamond & Pearl");
+define(__NAMESPACE__."_33", "Mysterious Treasures");
+define(__NAMESPACE__."_34", "Secret Wonders");
+define(__NAMESPACE__."_35", "Great Encounters");
+define(__NAMESPACE__."_36", "Majestic Dawn");
+define(__NAMESPACE__."_37", "Legends Awakened");
+define(__NAMESPACE__."_38", "Stormfront");
+define(__NAMESPACE__."_39", "Platinum");
+define(__NAMESPACE__."_40", "Rising Rivals");
+define(__NAMESPACE__."_41", "Supreme Victors");
+define(__NAMESPACE__."_42", "Arceus");
+define(__NAMESPACE__."_43", "HeartGold & SoulSilver");
+define(__NAMESPACE__."_44", "Unleashed")
+define(__NAMESPACE__."_45", "Undaunted")
+define(__NAMESPACE__."_46", "Triumphant")
+define(__NAMESPACE__."_47", "Call of Legends")
+define(__NAMESPACE__."_48", "Black & White")
+define(__NAMESPACE__."_49", "Emerging Powers")
+define(__NAMESPACE__."_50", "Noble Victories")
+define(__NAMESPACE__."_51", "Next Destinies")
+define(__NAMESPACE__."_52", "Dark Explorers")
+define(__NAMESPACE__."_53", "Dragon Exalted")
+define(__NAMESPACE__."_54", "Boundaries Crossed")
+define(__NAMESPACE__."_55", "Plasma Storm")
+define(__NAMESPACE__."_56", "Plasma Freeze")
+define(__NAMESPACE__."_57", "Plasma Blast")
+define(__NAMESPACE__."_58", "Legendary Treasures")
+define(__NAMESPACE__."_59", "XY")
+define(__NAMESPACE__."_60", "Flashfire")
+define(__NAMESPACE__."_61", "Furious Fists")
