6c3fbe2f29229e6ffaefa3738c1714befedacb5b
William Wright <william.alexandre.wright@gmail.com>
2014-07-31 14:32:17 -0400

    updated Card.class

--- code/ai-toolkit/classes/Card.class.php
+++ code/ai-toolkit/classes/Card.class.php
@@ -1,8 +1,7 @@
 <?php
 
-
 include 'EnergyType.ENUM.php';
-
+require_once('../lib/SetConstants.php');
 abstract class Card {
 	//attributes
 
@@ -36,23 +35,39 @@ abstract class Card {
         return $this->$rarity;
     }
 
-    #todo throw error is parameter is not a string
-    public function setName($name) {
+    public function setName(string $name) {
+        if(gettype($name) != "string") {
+            die("Sorry, but the function setName only accepts a string as a parameter");
+        }
+        if(preg_match('/^[a-zA-Z][#0-9]?$/', $rarity) != 1) {
+            die("I'm sorry, but ".$name." is not a valid pokemon name.");
+        }
         $this->$name = $name;
     }
 
-    #todo learn wtf is a set
+    #To have an idea which set to use please refer to /code/lib/SetConstants.php
     public function setSet($set) {
+        if(constant("SET_".$set) == null) {
+            die("not a valid set, please use the following syntax XX where XX is the number of the set that currently exist");
+        }
         $this->$set = $set;
     }
 
-    #todo learn wtf is a set number
-    public function setSetNumber($setNumber) {
+    #this probably means the card number in the set.
+    public function setSetNumber(int $setNumber) {
+        if(gettype($setNumber) != "integer") {
+            die("set number needs to be an integer.");
+        }
         $this->$setNumber = $setNumber;
     }
 
-    #todo learn more about this rarity, what chris actually means?
-    public function setRarity($rarity) {
+    public function setRarity(string $rarity) {
+        if(gettype($rarity) != "string") {
+            die ("rarity needs to be a string");
+        }
+        if(preg_match('/^(Common|Uncommon|Rare|Ultra Rare|Secret Rare)$/', ucwords($rarity)) != 1) {
+           die("I'm sorry, but ".$rarity." is a not a valid rarity term."); 
+        }
         $this->$rarity = $rarity;
     }
 
